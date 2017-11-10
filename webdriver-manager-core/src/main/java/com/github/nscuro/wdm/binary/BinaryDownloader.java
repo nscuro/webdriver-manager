97592f4fa32472d99eda92ad0aa2b6680d714133
nscuro <scuro@protonmail.com>
2017-11-10 21:51:08 +0100

    Add missing nonnull annotations

--- webdriver-manager-core/src/main/java/com/github/nscuro/wdm/binary/BinaryDownloader.java
+++ webdriver-manager-core/src/main/java/com/github/nscuro/wdm/binary/BinaryDownloader.java
@@ -19,10 +19,12 @@ public interface BinaryDownloader {
     @Nonnull
     File downloadLatest(final Os os, final Architecture architecture, final Path destinationDirPath) throws IOException;
 
+    @Nonnull
     default File download(final String version, final Path destinationDirPath) throws IOException {
         return download(version, Os.getCurrent(), Architecture.getCurrent(), destinationDirPath);
     }
 
+    @Nonnull
     default File downloadLatest(final Path destinationDirPath) throws IOException {
         return downloadLatest(Os.getCurrent(), Architecture.getCurrent(), destinationDirPath);
     }
