dd188d43daf4ac0f5364d10aa53356b4b1fc7a18
nscuro <scuro@protonmail.com>
2017-12-28 18:39:10 +0100

    Unit test for github release asset retrieval

--- webdriver-manager-core/src/test/java/com/github/nscuro/wdm/binary/github/GitHubReleasesServiceImplTest.java
+++ webdriver-manager-core/src/test/java/com/github/nscuro/wdm/binary/github/GitHubReleasesServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.github.nscuro.wdm.binary.github;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.nscuro.wdm.Platform;
 import org.apache.http.auth.Credentials;
 import org.apache.http.client.HttpClient;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,9 +10,12 @@ import org.junit.jupiter.api.Test;
 
 import javax.annotation.Nullable;
 
+import java.util.Collections;
 import java.util.Optional;
 
+import static java.util.Collections.singleton;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 class GitHubReleasesServiceImplTest {
@@ -25,6 +29,61 @@ class GitHubReleasesServiceImplTest {
         httpClient = mock(HttpClient.class);
 
         objectMapper = mock(ObjectMapper.class);
+    }
+
+    @Nested
+    class GetReleaseAssetForPlatformTest {
+
+        private GitHubReleasesService releasesService;
+
+        private Platform platform;
+
+        @BeforeEach
+        void beforeEach() {
+            releasesService = GitHubReleasesService.create(httpClient, objectMapper);
+
+            platform = mock(Platform.class);
+        }
+
+        @Test
+        void shouldReturnMatchingAsset() {
+            final GitHubReleaseAsset asset = new GitHubReleaseAsset();
+            asset.setName("my-asset-macosx64.zip");
+
+            final GitHubRelease release = new GitHubRelease();
+            release.setAssets(singleton(asset));
+
+            given(platform.getName())
+                    .willReturn("macosx64");
+
+            assertThat(releasesService.getReleaseAssetForPlatform(release, platform))
+                    .contains(asset);
+        }
+
+        @Test
+        void shouldReturnEmptyOptionalWhenNoAssetsArePresent() {
+            final GitHubRelease release = new GitHubRelease();
+            release.setAssets(Collections.emptySet());
+
+            assertThat(releasesService.getReleaseAssetForPlatform(release, platform))
+                    .isNotPresent();
+        }
+
+        @Test
+        void shouldReturnEmptyOptionalWhenNoAssetMatchesTheGivenPlatform() {
+            final GitHubReleaseAsset asset = new GitHubReleaseAsset();
+            asset.setName("my-asset-macosx64.zip");
+
+            final GitHubRelease release = new GitHubRelease();
+            release.setAssets(singleton(asset));
+
+            given(platform.getName())
+                    .willReturn("windows64");
+
+            assertThat(releasesService.getReleaseAssetForPlatform(release, platform))
+                    .isNotPresent();
+        }
+
     }
 
     @Nested
