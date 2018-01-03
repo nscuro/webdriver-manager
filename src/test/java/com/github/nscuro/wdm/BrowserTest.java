744c90c372b3bb18406e701d59123c8a22d7efff
nscuro <niklas.duester@protonmail.ch>
2018-01-03 19:59:18 +0100

    Add test for class names provided by Browser instances

--- src/test/java/com/github/nscuro/wdm/BrowserTest.java
+++ src/test/java/com/github/nscuro/wdm/BrowserTest.java
@@ -2,6 +2,7 @@ package com.github.nscuro.wdm;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.openqa.selenium.WebDriver;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,6 +13,13 @@ class BrowserTest {
     void shouldDefineBinarySystemProperty(final Browser browser) {
         assertThat(browser.getBinarySystemProperty())
                 .isNotNull();
+    }
+
+    @ParameterizedTest
+    @EnumSource(Browser.class)
+    void shouldDefineValidWebDriverClassName(final Browser browser) throws ClassNotFoundException {
+        assertThat(WebDriver.class)
+                .isAssignableFrom(Class.forName(browser.getWebDriverClassName()));
     }
 
 }
