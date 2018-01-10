d6bda26a04a4015029ada29cf2c9a18f37dd7dd3
nscuro <niklas.duester@protonmail.ch>
2018-01-10 19:39:47 +0100

    Make MicrosoftWebDriverRelease class package-private

--- src/main/java/com/github/nscuro/wdm/binary/edge/MicrosoftWebDriverRelease.java
+++ src/main/java/com/github/nscuro/wdm/binary/edge/MicrosoftWebDriverRelease.java
@@ -3,7 +3,7 @@ package com.github.nscuro.wdm.binary.edge;
 import lombok.Data;
 
 @Data
-public final class MicrosoftWebDriverRelease {
+final class MicrosoftWebDriverRelease {
 
     private final String version;
 
