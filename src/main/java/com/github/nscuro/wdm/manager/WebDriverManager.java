d9c3a4fb9b074db1e9b952557a2a5b0b6ca4f4c9
nscuro <niklas.duester@protonmail.ch>
2018-01-26 19:54:43 +0100

    Javadoc

--- src/main/java/com/github/nscuro/wdm/manager/WebDriverManager.java
+++ src/main/java/com/github/nscuro/wdm/manager/WebDriverManager.java
@@ -4,15 +4,28 @@ import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 
 import javax.annotation.Nonnull;
-import java.io.IOException;
 
 public interface WebDriverManager {
 
+    /**
+     * Get a {@link WebDriver} instance based on the given desired {@link Capabilities}.
+     *
+     * @param capabilities The desired {@link Capabilities}
+     * @return A {@link WebDriver} instance
+     */
     @Nonnull
-    WebDriver getWebDriver(final Capabilities capabilities) throws IOException;
+    WebDriver getWebDriver(final Capabilities capabilities);
 
+    /**
+     * Quit a given {@link WebDriver} instance.
+     *
+     * @param webDriver The {@link WebDriver} instance to quit
+     */
     void quitWebDriver(final WebDriver webDriver);
 
+    /**
+     * Quit all open {@link WebDriver} instances managed by this class.
+     */
     void shutdown();
 
 }
