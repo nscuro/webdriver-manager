8d133a4a13ef74c625fddf6d67f47a682e41ac02
nscuro <niklas.duester@protonmail.ch>
2018-02-04 20:58:29 +0100

    Add missing description of manager package

--- src/main/java/com/github/nscuro/wdm/manager/package-info.java
+++ src/main/java/com/github/nscuro/wdm/manager/package-info.java
@@ -1,3 +1,8 @@
+/**
+ * This package contains classes responsible for {@link org.openqa.selenium.WebDriver} instance management.
+ *
+ * @since 0.1.3
+ */
 @ParametersAreNonnullByDefault
 package com.github.nscuro.wdm.manager;
 
