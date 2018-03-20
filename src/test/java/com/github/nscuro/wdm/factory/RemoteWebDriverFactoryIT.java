479ad0f6dfee3d4d55c72542567c20b78f87942f
nscuro <niklas.duester@protonmail.ch>
2018-03-20 21:47:20 +0100

    Test documentation

--- src/test/java/com/github/nscuro/wdm/factory/RemoteWebDriverFactoryIT.java
+++ src/test/java/com/github/nscuro/wdm/factory/RemoteWebDriverFactoryIT.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.grid.internal.utils.configuration.StandaloneConfiguration;
 import org.openqa.selenium.WebDriver;
@@ -14,6 +15,7 @@ import java.util.Optional;
 
 import static java.lang.String.format;
 
+@DisplayName("The remote WebDriver factory")
 class RemoteWebDriverFactoryIT {
 
     private static SeleniumServer seleniumServer;
@@ -34,7 +36,8 @@ class RemoteWebDriverFactoryIT {
     }
 
     @Test
-    void shouldBeAbleToInitiateRemoteWebDriverInstances() {
+    @DisplayName("should be able to create remote WebDriver instances")
+    void shouldBeAbleToCreateRemoteWebDriverInstances() {
         webDriver = webDriverFactory.createWebDriver(DesiredCapabilities.htmlUnit());
     }
 
